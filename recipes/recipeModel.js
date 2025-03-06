ef2be807543d27f4dcf4d10826cedaac1e55231b
Trayan Stefanov <trayanstefanov95@gmail.com>
2025-03-06 20:02:01 +0200

    Extented Recipe Schema

--- recipes/recipeModel.js
+++ recipes/recipeModel.js
@@ -1,24 +1,41 @@
 import mongoose from "mongoose";
 
-const recipeSchema = mongoose.Schema({
-    title:{
-        type: String,
-        required: true,
+// Defining the schema for the Recipe model
+const recipeSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        summary: {
+            type: String,
+            required: true,
+        },
+        typeOfDish: {
+            type: String,
+            required: true,
+        },
+        ingredients: {
+            type: String,
+            required: true,
+        },
+        timeToCook: {
+            type: Number,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        imgDirPath: {
+            type: String,
+            required: true,
+        }
     },
-    ingredients:{
-        type: String,
-        required: true,
-    },
-    timeToCook:{
-        type: Number,
-        required: true,
-    },
-    description:{
-        type: String,
-        required: true,
-        unique: true,
-    }
-})
+    {
+        timestamps: true,
+    })
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
 export default Recipe;
