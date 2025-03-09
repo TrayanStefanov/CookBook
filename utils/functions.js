d455ce34122920efb663f61223274228da093501
Trayan Stefanov <trayanstefanov95@gmail.com>
2025-03-10 00:23:55 +0200

    Note

--- utils/functions.js
+++ utils/functions.js
@@ -1,3 +1,7 @@
+/*  Utility functions for the server.
+    Currently only contains a generic function to handle server errors.  
+    Note: Proper error handling needed.
+*/
 const handleServerError = (res, error) => {
     console.log(error);
     res.status(500).json({message: "Internal server error"});
