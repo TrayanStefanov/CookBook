6b3f2be71b5440d492f53e42e90ea4a804b6ee79
Trayan Stefanov <trayanstefanov95@gmail.com>
2025-03-10 00:22:28 +0200

    Added latest category route and function

--- recipes/recipeController.js
+++ recipes/recipeController.js
@@ -1,40 +1,56 @@
+/* 
+    This file contains the functions that will be called when the routes are used
+*/
+
 import mongoose from "mongoose";
 import Recipe from "./recipeModel.js";
 import { handleServerError } from "../utils/functions.js";
+import { console } from "inspector";
 
-const addRecipe = async(req, res) => {
-    try{
-        const {title, ingredients, timeToCook, description} = req.body;
-        if(!title || !ingredients || !timeToCook || !description) {
-            return res.status(400).json({message: "Incomplete Data"});
-        }
-
-        const recipeExists = await Recipe.findOne({description});
-        if(recipeExists){
-            return res.status(400).json({message: "Recipe already exists"});
-        }
-        const newRecipe = await Recipe.create({
-            title,
-            ingredients,
-            timeToCook,
-            description
-        });
-        res.status(201).json({
-            _id: newRecipe._id,
-            title: newRecipe.title,
-            ingredients: newRecipe.ingredients,
-            timeToCook: newRecipe.timeToCook,
-            description: newRecipe.description
-        });
+// Returns all recipes queried from DB. Sorts by creation date. Newest first.
+const listRecipes = async (req, res) => {
+    try {
+        // Change the sort variable to 1 for oldest first.
+        const recipes = await Recipe.find().sort({createdAt: -1});
+        res.status(200).json(recipes);
+        console.log(req.body);
     } catch (error){
         handleServerError(res, error);
     }
 };
 
-const listRecipes = async (req, res) => {
+// Returns recipes queried from DB based on search query. Case insensitive and exact string match.
+const searchRecipes = async (req, res) => {
     try {
-        const recipes = await Recipe.find({});
+        let request = JSON.stringify(req.query);
+        const recipes = await Recipe.find({ $text: { $search: request} });
         res.status(200).json(recipes);
+        console.log(res.body);
+    } catch (error){
+        handleServerError(res, error);
+    }
+}
+
+// Returns the 10 latest recipes queried from DB. Sorts by creation date. Newest first. 
+const latestRecipes = async (req, res) => {
+    try {
+        // Change the sort variable to 1 for oldest first. Change the limit to get more or less recipes.
+        const recipes = await Recipe.find().sort({createdAt: -1}).limit(10);
+        res.status(200).json(recipes);
+        console.log(req.body);
+    } catch (error){
+        handleServerError(res, error);
+    }
+};
+
+// Returns the 10 latest recipes by category queried from DB. Sorts by creation date. Newest first. 
+const latestCategoryRecipes = async (req, res) => {
+    try {
+        // Change the sort variable to 1 for oldest first. Change the limit to get more or less recipes.
+        let request = JSON.stringify(req.query);
+        const recipes = await Recipe.find({ $text: { $search: request} }).sort({createdAt: -1}).limit(10);
+        res.status(200).json(recipes);
+        console.log(req.body);
     } catch (error){
         handleServerError(res, error);
     }
@@ -42,50 +58,57 @@ const listRecipes = async (req, res) => {
 
 //Description needs to be different or validation picks it up due to being unique field
 
+//Reminder to make it impossible to edit a recipe by multiple people
+// Edits a recipe in the database. Requires a valid recipe ID.
 const editRecipe = async (req, res) => {
     try {
         const recipeId = req.params.id;
         const {title} = req.body;
+        const {summary} = req.body;
+        const {typeOfDish} = req.body;
         const {ingredients} = req.body;
         const {timeToCook} = req.body;
         const {description} = req.body;
         
-        if(!title && !ingredients && !timeToCook && !description){
+        // Checks if any of the required fields are missing.
+        if(!title && !summary && !typeOfDish && !ingredients && !timeToCook && !description){
             return res.status(400).json({message: "Incomplete Data"})
         }
+        // Checks if the recipe ID is valid.
         if(!mongoose.Types.ObjectId.isValid(recipeId)){
             return res.status(400).json({message: "Invalid recipe ID"});
         }
 
+        // Finds the recipe in the daзззtabase. Overwrites the recipe data.
         const recipe = await Recipe.findById(recipeId);
-        if(!recipe){
-            return res.status(404).json({message: "Recipe not found"});
-        }
-
         recipe.title = title;
+        recipe.summary = summary;
+        recipe.typeOfDish = typeOfDish;
         recipe.ingredients = ingredients;
         recipe.timeToCook = timeToCook;
         recipe.description = description;
-        
+        // Save the updated recipe to the database.
         await recipe.save();
+        // Return the updated recipe.
         res.status(200).json(recipe);
     } catch (error){
         handleServerError(res, error);
     }
 };
 
+// Deletes a recipe from the database. Requires a valid recipe ID.
 const deleteRecipe = async (req, res) => {
     try {
         const recipeId = req.params.id;
         if(!mongoose.Types.ObjectId.isValid(recipeId)){
             return res.status(400).json({message: "Invalid recipe ID"});
         }
-
+        // Checks if the recipe ID is valid.
         const recipe = await Recipe.findById(recipeId);
         if(!recipe){
             return res.status(404).json({message: "Recipe not found"});
         }
-
+        // Deletes the recipe from the database.
         await Recipe.findByIdAndDelete({_id: recipeId});
         res.status(200).json({message: "Recipe deleted"});
     } catch (error){
@@ -93,4 +116,6 @@ const deleteRecipe = async (req, res) => {
     }
 };
 
-export {addRecipe, listRecipes, editRecipe, deleteRecipe};
+
+
+export {listRecipes, searchRecipes, latestRecipes, latestCategoryRecipes, editRecipe, deleteRecipe};
--- recipes/recipeRoutes.js
+++ recipes/recipeRoutes.js
@@ -1,10 +1,19 @@
-import express from 'express';
-import { addRecipe, listRecipes, editRecipe, deleteRecipe } from "./recipeController.js";
+/*
+    This is the server's router for the application.
+    All routes for the applicatiion's handling of recipes are defined here.
+*/
 
+import express, { Router } from 'express';
+import {listRecipes, searchRecipes, latestRecipes, latestCategoryRecipes, editRecipe, deleteRecipe} from "./recipeController.js";
+
+// Creating a new router
 const router = express.Router();
 
-router.post('/addRecipe' , addRecipe);
+// Setting up the routes for the different website paths.
 router.get('/listRecipes', listRecipes);
+router.get('/searchRecipes', searchRecipes);
+router.get('/latestRecipes', latestRecipes);
+router.get('/latestCategoryRecipes', latestCategoryRecipes);
 router.put('/:id', editRecipe);
 router.delete('/:id', deleteRecipe);
 
